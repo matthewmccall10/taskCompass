361f6635208391b7fe15b74020bf0e41e8a88fd7
Thomas Zimnick <thomas.zimnick@gmail.com>
2024-12-04 12:22:22 -0500

    Initial menu setup

--- main.java
+++ main.java
@@ -2,12 +2,25 @@ import java.util.Scanner;
 
 class test {
     public static void main(String args[]) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your name:");
-        String name = scanner.nextLine();
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println("Hello " + name);
+        boolean menuStop = false;
+        
+        //while loop to run until exit is selected
+        while(menuStop == false) {
+            //printing options menu
+            System.out.println("Welcome to the bookstore, please select an option below:");
+            System.out.println("\t1: Check Inventory");
+            System.out.println("\t2: Add Member");
+            System.out.println("\t3: Make Purchase");
+            System.out.println("\t4: Check Customer's Total Spent");
+            System.out.println("\t5: Check Premium Fee Payment");
+            System.out.println("\t6: Restock Product");
+            System.out.println("\t7: Check Inventory Value");
+            System.out.println("\t8: Compare Product Prices");
+            System.out.println("\t9: Exit");
+        }
 
-        scanner.close();
+        sc.close();
     }
 }
