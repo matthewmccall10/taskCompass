57eb5f438cde65adb30eb4b9a123ba7242255c49
Thomas Zimnick <thomas.zimnick@gmail.com>
2024-12-04 16:43:33 -0500

    Finished generic task creation

--- task.java
+++ task.java
@@ -2,18 +2,45 @@ public class task {
 
     public int taskID;
     public String taskName;
+    public String taskUser;
+    public String taskDescription;
+    // public Date TBD;
+    public String taskPriority;
+    public boolean taskStatus;
+
     //More need to be added
     
     //Constructor
-    public task (int nextID, String name, String user) {
+    public task (int nextID, String name, String user, String description, String priority, boolean status) {
         this.taskID = nextID;
         this.taskName = name;
-        //More need to be added
+        this.taskUser = user;
+        this.taskDescription = description;
+        // this.taskDate = date;
+        this.taskPriority = priority;
+        this.taskStatus = status;
+        
     }
 
     //Getters
     public String getTaskName() {
         return taskName;
+    }
+
+    public String getTaskUser() {
+        return taskUser;
+    }
+
+    public String getTaskDescription() {
+        return taskDescription;
+    }
+
+    public String getTaskPriority() {
+        return taskPriority;
+    }
+
+    public boolean getTaskStatus() {
+        return taskStatus;
     }
 
     //Setters
